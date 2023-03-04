dbb15fb573f6d42045d493ca35d3259d45041db8
Zikri21 <112067827+Zikri21@users.noreply.github.com>
2023-03-04 08:46:36 +0700

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -1,9 +1,60 @@
 #!/bin/bash
-# User Trial SSH / OPENVPN
-# Edition : Stable Edition V1.0
-# Auther  : AWALUDIN FERIYANTO
-# (C) Copyright 2021-2022 By FSIDVPN
-# =========================================
+BURIQ () {
+    curl -sS https://raw.githubusercontent.com/Zikri21/permission/main/ipmini > /root/tmp
+    data=( `cat /root/tmp | grep -E "^### " | awk '{print $2}'` )
+    for user in "${data[@]}"
+    do
+    exp=( `grep -E "^### $user" "/root/tmp" | awk '{print $3}'` )
+    d1=(`date -d "$exp" +%s`)
+    d2=(`date -d "$biji" +%s`)
+    exp2=$(( (d1 - d2) / 86400 ))
+    if [[ "$exp2" -le "0" ]]; then
+    echo $user > /etc/.$user.ini
+    else
+    rm -f /etc/.$user.ini > /dev/null 2>&1
+    fi
+    done
+    rm -f /root/tmp
+}
+
+MYIP=$(curl -sS ipv4.icanhazip.com)
+Name=$(curl -sS https://raw.githubusercontent.com/Zikri21/permission/main/ipmini | grep $MYIP | awk '{print $2}')
+echo $Name > /usr/local/etc/.$Name.ini
+CekOne=$(cat /usr/local/etc/.$Name.ini)
+
+Bloman () {
+if [ -f "/etc/.$Name.ini" ]; then
+CekTwo=$(cat /etc/.$Name.ini)
+    if [ "$CekOne" = "$CekTwo" ]; then
+        res="Expired"
+    fi
+else
+res="Permission Accepted..."
+fi
+}
+
+PERMISSION () {
+    MYIP=$(curl -sS ipv4.icanhazip.com)
+    IZIN=$(curl -sS https://raw.githubusercontent.com/Zikri21/permission/main/ipmini | awk '{print $4}' | grep $MYIP)
+    if [ "$MYIP" = "$IZIN" ]; then
+    Bloman
+    else
+    res="Permission Denied!"
+    fi
+    BURIQ
+}
+red='\e[1;31m'
+green='\e[1;32m'
+NC='\e[0m'
+green() { echo -e "\\033[32;1m${*}\\033[0m"; }
+red() { echo -e "\\033[31;1m${*}\\033[0m"; }
+PERMISSION
+
+if [ "$res" = "Expired" ]; then
+Exp="\e[36mExpired\033[0m"
+else
+Exp=$(curl -sS https://raw.githubusercontent.com/Zikri21/permission/main/ipmini | grep $MYIP | awk '{print $3}')
+fi
 
 # // Exporting Language to UTF-8
 export LC_ALL='en_US.UTF-8'
@@ -241,9 +292,9 @@ echo -e "\e[36m╘════════════════════�
 "
 echo -e   " \033[1;33m Press x or [ Ctrl+C ] • To-Exit-Script ${NC}"
 echo -e  "\e[36m╒════════════════════════════════════════════════╕\033[0m"
-echo -e "\e[36m│ Client Name :$NC \033[1;32mPREMIUM SCRIPT MULTI PORT ${NC}"
+echo -e "\e[36m│ Client Name :$NC \033[1;32m $Name ${NC}"
 echo -e "\e[36m│ User Roles  :${NC} \033[1;32mPremium user ${NC}"
-    echo -e "\e[36m│ Exp License :$NC \033[1;32mLIFETIME ${NC}"
+    echo -e "\e[36m│ Exp License :$NC \033[1;32m $Exp Day ${NC}"
 echo -e "\e[36m╘════════════════════════════════════════════════╛\033[0m"
 echo -e   ""
 read -p " Select menu :  "  opt
